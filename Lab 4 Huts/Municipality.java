10c35e0b603a79bd911a7d4ed0c16bc8b07015a3
Serena Rocchio <serenarocc@gmail.com>
2023-10-09 08:57:55 +0000

    Upload New File

---
+++ Lab 4 Huts/Municipality.java
@@ -0,0 +1,31 @@
+package mountainhuts;
+
+/**
+ * Class representing a municipality that hosts a mountain hut.
+ * It is a data class with getters for name, province, and altitude
+ * 
+ */
+public class Municipality {
+	private final String name;
+	private final String province;
+	private final Integer altitude;
+
+	public Municipality(String name, String province, Integer altitude) {
+		this.name = name;
+		this.province = province;
+		this.altitude = altitude;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public Integer getAltitude() {
+		return altitude;
+	}
+
+}
